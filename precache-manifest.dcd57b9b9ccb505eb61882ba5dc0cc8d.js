af0fe9159b248a8687059b5fb4c26acec3da0ac2
Caio Teixeira <tex.caio@gmail.com>
2019-12-06 17:56:48 -0300

    Updates

---
+++ precache-manifest.dcd57b9b9ccb505eb61882ba5dc0cc8d.js
@@ -0,0 +1,510 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f85ebcb2c6beab81662ea61ef70163",
+    "url": "/caioteixeiraa/site-neu/index.html"
+  },
+  {
+    "revision": "a6f937e99f97e9ef6275",
+    "url": "/caioteixeiraa/site-neu/static/css/2.8063dab7.chunk.css"
+  },
+  {
+    "revision": "05d09f762ab47952baa4",
+    "url": "/caioteixeiraa/site-neu/static/css/main.f148ef39.chunk.css"
+  },
+  {
+    "revision": "a6f937e99f97e9ef6275",
+    "url": "/caioteixeiraa/site-neu/static/js/2.4b903933.chunk.js"
+  },
+  {
+    "revision": "05d09f762ab47952baa4",
+    "url": "/caioteixeiraa/site-neu/static/js/main.477b9963.chunk.js"
+  },
+  {
+    "revision": "4dba2bd23ab8fe3ba6fd",
+    "url": "/caioteixeiraa/site-neu/static/js/runtime~main.a1b1d20f.js"
+  },
+  {
+    "revision": "86fc61ffbdb6cd7c5978ae390b4c274c",
+    "url": "/caioteixeiraa/site-neu/static/media/BELEZA-WEB.86fc61ff.png"
+  },
+  {
+    "revision": "b8030119f826c8b080ae75a5b451d91a",
+    "url": "/caioteixeiraa/site-neu/static/media/Deborah.b8030119.png"
+  },
+  {
+    "revision": "b722645488851c49dbc935ae304bd759",
+    "url": "/caioteixeiraa/site-neu/static/media/Ela líder-02.b7226454.svg"
+  },
+  {
+    "revision": "eff7c12d3a52bfca30d813a26385ef1b",
+    "url": "/caioteixeiraa/site-neu/static/media/FlowsenseLogo.eff7c12d.svg"
+  },
+  {
+    "revision": "e07c62f9d58bbc53814b4ea084fb9da8",
+    "url": "/caioteixeiraa/site-neu/static/media/Lean-survey.e07c62f9.jpg"
+  },
+  {
+    "revision": "65dafe5893d01080ace608a695dd4426",
+    "url": "/caioteixeiraa/site-neu/static/media/Liv UP.65dafe58.png"
+  },
+  {
+    "revision": "8dc372f1638be2ffa68f92c453e2179b",
+    "url": "/caioteixeiraa/site-neu/static/media/Logo Taqtile.8dc372f1.svg"
+  },
+  {
+    "revision": "21f91dadcabc54b244a4b521976fc09a",
+    "url": "/caioteixeiraa/site-neu/static/media/Logo-piratas.21f91dad.svg"
+  },
+  {
+    "revision": "40577799b93e4035ced6d6255fe42dea",
+    "url": "/caioteixeiraa/site-neu/static/media/Logo-piratas.40577799.png"
+  },
+  {
+    "revision": "b8a2ee8ec9ef744ffc0d1f5337724b18",
+    "url": "/caioteixeiraa/site-neu/static/media/Logo_Hack.b8a2ee8e.png"
+  },
+  {
+    "revision": "50b665c751e6cb98761814286edb3285",
+    "url": "/caioteixeiraa/site-neu/static/media/LucasPrado.50b665c7.png"
+  },
+  {
+    "revision": "a3b387c93882604792867736aecd56c8",
+    "url": "/caioteixeiraa/site-neu/static/media/Montserrat-Bold.a3b387c9.ttf"
+  },
+  {
+    "revision": "2f354053d1c9c36f533ebd226698bfa5",
+    "url": "/caioteixeiraa/site-neu/static/media/Montserrat-Light.2f354053.ttf"
+  },
+  {
+    "revision": "0098f804fc2d06af52650e0b8ed3390c",
+    "url": "/caioteixeiraa/site-neu/static/media/Montserrat-Medium.0098f804.ttf"
+  },
+  {
+    "revision": "a8a117360e71de94ae3b0b0f8d15b44d",
+    "url": "/caioteixeiraa/site-neu/static/media/Montserrat-Regular.a8a11736.ttf"
+  },
+  {
+    "revision": "afc3f701b6d7bb1c39b92ef99de105da",
+    "url": "/caioteixeiraa/site-neu/static/media/NU BANK.afc3f701.png"
+  },
+  {
+    "revision": "a2816b09af8b1d96033ab9c22674379a",
+    "url": "/caioteixeiraa/site-neu/static/media/Nubank_Logo.a2816b09.eps"
+  },
+  {
+    "revision": "ad343063c545415979e738e4f286f48e",
+    "url": "/caioteixeiraa/site-neu/static/media/Nubank_Logo.ad343063.png"
+  },
+  {
+    "revision": "8b7b796f64f386c11ec3bf2fda043e9b",
+    "url": "/caioteixeiraa/site-neu/static/media/Oxiot_logo.8b7b796f.png"
+  },
+  {
+    "revision": "211672313f8ac2fb18f61751d4d99303",
+    "url": "/caioteixeiraa/site-neu/static/media/PRP.21167231.png"
+  },
+  {
+    "revision": "8083c61618302f584c758df49276283f",
+    "url": "/caioteixeiraa/site-neu/static/media/Retina_vision_preto_low.8083c616.png"
+  },
+  {
+    "revision": "c4d1e41808b5b8f8ac007c12cf93b565",
+    "url": "/caioteixeiraa/site-neu/static/media/Thabata.c4d1e418.png"
+  },
+  {
+    "revision": "2fda8dd4affb550f16b409ecfc612bc2",
+    "url": "/caioteixeiraa/site-neu/static/media/VERTICORE_LOGO.2fda8dd4.png"
+  },
+  {
+    "revision": "460cbc3f2427f745d839055761376114",
+    "url": "/caioteixeiraa/site-neu/static/media/WORLDPACKERS.460cbc3f.png"
+  },
+  {
+    "revision": "eb3cb9fdfbeb052018f7743d66e7fb21",
+    "url": "/caioteixeiraa/site-neu/static/media/agenda.eb3cb9fd.svg"
+  },
+  {
+    "revision": "404e7fc1000da119e10862f87df26db7",
+    "url": "/caioteixeiraa/site-neu/static/media/appsimples_logo.404e7fc1.svg"
+  },
+  {
+    "revision": "c779a780597ac139792329a1c254e8a3",
+    "url": "/caioteixeiraa/site-neu/static/media/b2b.c779a780.png"
+  },
+  {
+    "revision": "0fd6ca23937b5349f9b6cb5c59e02ce7",
+    "url": "/caioteixeiraa/site-neu/static/media/banner-lab.0fd6ca23.png"
+  },
+  {
+    "revision": "2516a4ce03a6dfd22346f7fbe50e0dc5",
+    "url": "/caioteixeiraa/site-neu/static/media/banner-ship.2516a4ce.png"
+  },
+  {
+    "revision": "2ffe4ffe28b1337713d1d2c987f393a1",
+    "url": "/caioteixeiraa/site-neu/static/media/baseline-arrow_right_alt-24px.2ffe4ffe.svg"
+  },
+  {
+    "revision": "87fbef26752cd987a064411600cdce99",
+    "url": "/caioteixeiraa/site-neu/static/media/beleza na web.87fbef26.svg"
+  },
+  {
+    "revision": "9b1a5174f74a611ad7d416fdce79f187",
+    "url": "/caioteixeiraa/site-neu/static/media/caos_focado-01.9b1a5174.svg"
+  },
+  {
+    "revision": "ed6f49cd15ad80434df7fb2244138392",
+    "url": "/caioteixeiraa/site-neu/static/media/caos_focado.ed6f49cd.png"
+  },
+  {
+    "revision": "b7a9d425e7691bd875524593fd75fe40",
+    "url": "/caioteixeiraa/site-neu/static/media/celebrar.b7a9d425.png"
+  },
+  {
+    "revision": "4776b10cdac1fb87d67962396f2286e6",
+    "url": "/caioteixeiraa/site-neu/static/media/collaboration.4776b10c.svg"
+  },
+  {
+    "revision": "90c8f376b0527af49c99d6f8bfa47fc6",
+    "url": "/caioteixeiraa/site-neu/static/media/criacao-startups.90c8f376.png"
+  },
+  {
+    "revision": "c6c81c8554acebc3a17d28b7e87d3ace",
+    "url": "/caioteixeiraa/site-neu/static/media/cuidas-logo.c6c81c85.svg"
+  },
+  {
+    "revision": "b07484e0899012a4083c16a5254c8d25",
+    "url": "/caioteixeiraa/site-neu/static/media/diego.b07484e0.png"
+  },
+  {
+    "revision": "f06087db3e4408654943378288ba485b",
+    "url": "/caioteixeiraa/site-neu/static/media/download-arrow.f06087db.svg"
+  },
+  {
+    "revision": "a704e5523a00709f0bc04285364cfd31",
+    "url": "/caioteixeiraa/site-neu/static/media/e-book-min.a704e552.png"
+  },
+  {
+    "revision": "66cdcd09d346032310dc539e223f65bb",
+    "url": "/caioteixeiraa/site-neu/static/media/education.66cdcd09.svg"
+  },
+  {
+    "revision": "bc906efd4b899403c77c8cbaf9bc88d5",
+    "url": "/caioteixeiraa/site-neu/static/media/evento-min.bc906efd.png"
+  },
+  {
+    "revision": "885f38657342d81f525556eecf2751cd",
+    "url": "/caioteixeiraa/site-neu/static/media/foto-inova-2.885f3865.jpg"
+  },
+  {
+    "revision": "6677ba778b388cfab0cfad2c6564d612",
+    "url": "/caioteixeiraa/site-neu/static/media/foto-inova.6677ba77.jpg"
+  },
+  {
+    "revision": "d8ea7323b309e3fb5c1595c621a8c2aa",
+    "url": "/caioteixeiraa/site-neu/static/media/happiness.d8ea7323.svg"
+  },
+  {
+    "revision": "ab56f50652212e96cd3e3cc9359eea84",
+    "url": "/caioteixeiraa/site-neu/static/media/header-cursos.ab56f506.png"
+  },
+  {
+    "revision": "ae4a54bbf61174a5b24f0a400afb748d",
+    "url": "/caioteixeiraa/site-neu/static/media/home_StartupLAB.ae4a54bb.png"
+  },
+  {
+    "revision": "5db404b0656e2b27616139a60dd4c55f",
+    "url": "/caioteixeiraa/site-neu/static/media/home_StartupSHIP.5db404b0.png"
+  },
+  {
+    "revision": "5b6dfd19262d420559d6e93ff9ef540e",
+    "url": "/caioteixeiraa/site-neu/static/media/home_banner.5b6dfd19.png"
+  },
+  {
+    "revision": "a105e1a71511f08e77d5260ecbd99abe",
+    "url": "/caioteixeiraa/site-neu/static/media/home_ebook.a105e1a7.png"
+  },
+  {
+    "revision": "7994a10be88602c225304078fe635dc5",
+    "url": "/caioteixeiraa/site-neu/static/media/hugo.7994a10b.png"
+  },
+  {
+    "revision": "932e03d1fe07ba195c2cb37ab6888c68",
+    "url": "/caioteixeiraa/site-neu/static/media/icn_data-science.932e03d1.svg"
+  },
+  {
+    "revision": "f3360ae461805f5354b6cf6ad778b265",
+    "url": "/caioteixeiraa/site-neu/static/media/icn_interface-design.f3360ae4.svg"
+  },
+  {
+    "revision": "1bc95dca3cf17faf0241e650578170c1",
+    "url": "/caioteixeiraa/site-neu/static/media/icon_marketing.1bc95dca.svg"
+  },
+  {
+    "revision": "c7049f5d102687101bfe5946aebdc81b",
+    "url": "/caioteixeiraa/site-neu/static/media/ifood-03.c7049f5d.svg"
+  },
+  {
+    "revision": "1febd0884b8872b6a8d3e74cd82d7ca1",
+    "url": "/caioteixeiraa/site-neu/static/media/ifood-logo.1febd088.png"
+  },
+  {
+    "revision": "c3ad365df26aeb4551244cad731cb5e1",
+    "url": "/caioteixeiraa/site-neu/static/media/ingresse-06.c3ad365d.svg"
+  },
+  {
+    "revision": "e423c6662d34567dccbbe3917e94d462",
+    "url": "/caioteixeiraa/site-neu/static/media/ingresse.e423c666.png"
+  },
+  {
+    "revision": "f302686cfd44123019362b792d3b2914",
+    "url": "/caioteixeiraa/site-neu/static/media/jurídico.f302686c.png"
+  },
+  {
+    "revision": "58c1d842950c9aae78818c208b9d9be7",
+    "url": "/caioteixeiraa/site-neu/static/media/lightbulb.58c1d842.svg"
+  },
+  {
+    "revision": "9d0356f606ef6208077e638924331d5c",
+    "url": "/caioteixeiraa/site-neu/static/media/like.9d0356f6.svg"
+  },
+  {
+    "revision": "9b9f2de331f4358b3cd76d5ac9b4c2df",
+    "url": "/caioteixeiraa/site-neu/static/media/liv up-04.9b9f2de3.svg"
+  },
+  {
+    "revision": "5b2cbdd8ab6fe7ac8305b4bcb8f2435d",
+    "url": "/caioteixeiraa/site-neu/static/media/logo-99--black-yellow.5b2cbdd8.svg"
+  },
+  {
+    "revision": "87fbef26752cd987a064411600cdce99",
+    "url": "/caioteixeiraa/site-neu/static/media/logo-blz.87fbef26.svg"
+  },
+  {
+    "revision": "6e8919fcb4f813084a4e310848934eaf",
+    "url": "/caioteixeiraa/site-neu/static/media/logo-colab.6e8919fc.png"
+  },
+  {
+    "revision": "b94da0b0c375faed5b3e7d5bfce7d580",
+    "url": "/caioteixeiraa/site-neu/static/media/logo-espresso.b94da0b0.png"
+  },
+  {
+    "revision": "187db09d238776145caeadd8ec4a208d",
+    "url": "/caioteixeiraa/site-neu/static/media/logo-genera.187db09d.png"
+  },
+  {
+    "revision": "187db09d238776145caeadd8ec4a208d",
+    "url": "/caioteixeiraa/site-neu/static/media/logo-genera1-e1458431074399.187db09d.png"
+  },
+  {
+    "revision": "07dfb648c711b6a436f01c0be0ac6707",
+    "url": "/caioteixeiraa/site-neu/static/media/logo-getninjas.07dfb648.png"
+  },
+  {
+    "revision": "24bbfbeb476305e1380393368e88824a",
+    "url": "/caioteixeiraa/site-neu/static/media/logo-hangar.24bbfbeb.svg"
+  },
+  {
+    "revision": "1e3066d59fc69df15a6bb36946e4fa20",
+    "url": "/caioteixeiraa/site-neu/static/media/logo-idwall.1e3066d5.svg"
+  },
+  {
+    "revision": "8048eba6425b7a12a38c688276e33cee",
+    "url": "/caioteixeiraa/site-neu/static/media/logo-ingresse.8048eba6.png"
+  },
+  {
+    "revision": "76af27fc0214febe41092c9c4b0a16ad",
+    "url": "/caioteixeiraa/site-neu/static/media/logo-lab.76af27fc.png"
+  },
+  {
+    "revision": "f7a249e7c9832620ba968480643d634e",
+    "url": "/caioteixeiraa/site-neu/static/media/logo-looqbox.f7a249e7.svg"
+  },
+  {
+    "revision": "481efd50573a5b3d87a2dd2d487657e8",
+    "url": "/caioteixeiraa/site-neu/static/media/logo-mobly.481efd50.svg"
+  },
+  {
+    "revision": "89b39a4148b762b93d6de22f1a997387",
+    "url": "/caioteixeiraa/site-neu/static/media/logo-mvisia.89b39a41.svg"
+  },
+  {
+    "revision": "3988c9677f82e50c959bdef949fe54e5",
+    "url": "/caioteixeiraa/site-neu/static/media/logo-revelo.3988c967.svg"
+  },
+  {
+    "revision": "2e1bd2536a0da05614fa7cacdca2236f",
+    "url": "/caioteixeiraa/site-neu/static/media/logo-ser.2e1bd253.jpeg"
+  },
+  {
+    "revision": "bf4678ffc86d03a42f47583585517d67",
+    "url": "/caioteixeiraa/site-neu/static/media/logo-ship.bf4678ff.png"
+  },
+  {
+    "revision": "0cbedce08ad37420828d4276d4bc0c5b",
+    "url": "/caioteixeiraa/site-neu/static/media/logo-stone.0cbedce0.png"
+  },
+  {
+    "revision": "ee430489d1505483166c19ab9ed00d4e",
+    "url": "/caioteixeiraa/site-neu/static/media/logo-uber.ee430489.png"
+  },
+  {
+    "revision": "95770e61500ac10b665c13f1d10695cd",
+    "url": "/caioteixeiraa/site-neu/static/media/logo-uplexis.95770e61.png"
+  },
+  {
+    "revision": "7048bb94078400406c436b25aed87834",
+    "url": "/caioteixeiraa/site-neu/static/media/logo-verticore.7048bb94.png"
+  },
+  {
+    "revision": "7ee94c7478574f5b252c3789fcaaaf50",
+    "url": "/caioteixeiraa/site-neu/static/media/logoNEU-sec.7ee94c74.png"
+  },
+  {
+    "revision": "feb5ffb569b09e486d84e42387faab50",
+    "url": "/caioteixeiraa/site-neu/static/media/logo_startup-lab-1.feb5ffb5.svg"
+  },
+  {
+    "revision": "29555ec16cc2fab25a238f28420541bc",
+    "url": "/caioteixeiraa/site-neu/static/media/logo_startup-lab.29555ec1.svg"
+  },
+  {
+    "revision": "7108f42a4316bee1e4540a0f319ec0f0",
+    "url": "/caioteixeiraa/site-neu/static/media/logo_startup-lab2.7108f42a.png"
+  },
+  {
+    "revision": "d3e37dbe12c31336eb3344166a35252f",
+    "url": "/caioteixeiraa/site-neu/static/media/megaphone.d3e37dbe.svg"
+  },
+  {
+    "revision": "cd359c5c45c722e33d7519efae5d1a6b",
+    "url": "/caioteixeiraa/site-neu/static/media/mkt-digital.cd359c5c.png"
+  },
+  {
+    "revision": "2a09ad5fc227f5b317de7a20a2ae30e9",
+    "url": "/caioteixeiraa/site-neu/static/media/mkt.2a09ad5f.svg"
+  },
+  {
+    "revision": "6108fc10c63e359c566950402bd722b8",
+    "url": "/caioteixeiraa/site-neu/static/media/monitor.6108fc10.svg"
+  },
+  {
+    "revision": "e7fe06e8a9f16b7eb655996c6d90caca",
+    "url": "/caioteixeiraa/site-neu/static/media/monitor.e7fe06e8.png"
+  },
+  {
+    "revision": "08be9bcef3a1f0b6820d523f59fe2a52",
+    "url": "/caioteixeiraa/site-neu/static/media/network.08be9bce.svg"
+  },
+  {
+    "revision": "651d0641c25c456b28ee71a5973a65a4",
+    "url": "/caioteixeiraa/site-neu/static/media/notebook.651d0641.svg"
+  },
+  {
+    "revision": "7cdea023551239d3bf86c1dd09466bea",
+    "url": "/caioteixeiraa/site-neu/static/media/nubank-05.7cdea023.svg"
+  },
+  {
+    "revision": "598f6193265c4a66ec5759bdbc8d5746",
+    "url": "/caioteixeiraa/site-neu/static/media/operacao-escala.598f6193.png"
+  },
+  {
+    "revision": "916e44940f3b3eb538c3f1f222d3a8ed",
+    "url": "/caioteixeiraa/site-neu/static/media/pier final-01.916e4494.png"
+  },
+  {
+    "revision": "49666d12ec7d54099467f347c2f94d1f",
+    "url": "/caioteixeiraa/site-neu/static/media/placeholder-outline.49666d12.svg"
+  },
+  {
+    "revision": "52cc19ace2c175bbfcc7efabe135e881",
+    "url": "/caioteixeiraa/site-neu/static/media/recycle.52cc19ac.svg"
+  },
+  {
+    "revision": "6bdca495102bfd0ea1ec0fbb66308381",
+    "url": "/caioteixeiraa/site-neu/static/media/renato.6bdca495.png"
+  },
+  {
+    "revision": "d766ec77d7f55579866cb06830d463b7",
+    "url": "/caioteixeiraa/site-neu/static/media/sato.d766ec77.png"
+  },
+  {
+    "revision": "40562b320b9e169b32720820ea39643f",
+    "url": "/caioteixeiraa/site-neu/static/media/screencapture-google-maps-place-InovaLab-Poli-23-5570386-46-7320631-18z-data-4m5-3m4-1s0x0-0xcac47aef5f2195ef-8m2-3d-23-5567047-4d-46-7331657-2019-03-.40562b32.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/caioteixeiraa/site-neu/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/caioteixeiraa/site-neu/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/caioteixeiraa/site-neu/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/caioteixeiraa/site-neu/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "95b82a75bfd78a70028ff68f45f22976",
+    "url": "/caioteixeiraa/site-neu/static/media/speech.95b82a75.svg"
+  },
+  {
+    "revision": "4b1d1b200e3f415134e72ffa176bc548",
+    "url": "/caioteixeiraa/site-neu/static/media/startups-rede.4b1d1b20.png"
+  },
+  {
+    "revision": "53199a57801f51ffc90e0bd94346988a",
+    "url": "/caioteixeiraa/site-neu/static/media/storymax_72dpi_web.53199a57.png"
+  },
+  {
+    "revision": "40ff95723804bf80c27271ec94d1289f",
+    "url": "/caioteixeiraa/site-neu/static/media/talk.40ff9572.svg"
+  },
+  {
+    "revision": "11109ff0eb651cf64e7689808110700f",
+    "url": "/caioteixeiraa/site-neu/static/media/two-circular.11109ff0.svg"
+  },
+  {
+    "revision": "bd2bbd1917a5c6309dc7fccec306eb99",
+    "url": "/caioteixeiraa/site-neu/static/media/user.bd2bbd19.svg"
+  },
+  {
+    "revision": "96f2fcfd812f628de0dd94e6cfa6274c",
+    "url": "/caioteixeiraa/site-neu/static/media/usp-1-logo-svg-vector.96f2fcfd.svg"
+  },
+  {
+    "revision": "8fbf38412fbb8f20548d81197114f1bc",
+    "url": "/caioteixeiraa/site-neu/static/media/ux_ui.8fbf3841.png"
+  },
+  {
+    "revision": "faa1e5afe4bf32d1c0056f4a3ca77757",
+    "url": "/caioteixeiraa/site-neu/static/media/video-player.faa1e5af.svg"
+  },
+  {
+    "revision": "68bc8f9dabe5841de0a26569b3bb4540",
+    "url": "/caioteixeiraa/site-neu/static/media/video.68bc8f9d.svg"
+  },
+  {
+    "revision": "9c43e9fe74d84a42f35d3122c7f833f0",
+    "url": "/caioteixeiraa/site-neu/static/media/vr_monkey.9c43e9fe.jpg"
+  },
+  {
+    "revision": "db034fc69149fa5c8442c6a00bd4cb47",
+    "url": "/caioteixeiraa/site-neu/static/media/working-briefcase.db034fc6.svg"
+  },
+  {
+    "revision": "db5e77fc88d1273eab799453b8eba072",
+    "url": "/caioteixeiraa/site-neu/static/media/yellow-logo-sharing.db5e77fc.png"
+  },
+  {
+    "revision": "8c09c799c83431b42d915a3db2e85fb0",
+    "url": "/caioteixeiraa/site-neu/static/media/yellowlogo.8c09c799.png"
+  },
+  {
+    "revision": "df54a2928c3dd8f26b2ecd36cc9f7cc2",
+    "url": "/caioteixeiraa/site-neu/static/media/yshay.df54a292.png"
+  }
+]);
